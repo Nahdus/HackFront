e326db5de45861f1ccec1ee4335997115af13d33
Sudhan <25459246+Nahdus@users.noreply.github.com>
2024-09-25 07:24:46 +0530

    added poission calc

--- src/components/poissonDistribution.js
+++ src/components/poissonDistribution.js
@@ -32,24 +32,60 @@ function poissonProbability(lambda, k) {
   return (Math.pow(lambda, k) * Math.exp(-lambda)) / factorial(k);
 }
 
-function PoissonGraph({ lambda }) {
+function PoissonGraph() {
+
+
   const [probabilityData, setProbabilityData] = useState([]);
+  const [numberOfTickets, setNumberOfTickets] = useState(0)
+  const [startTime,setStartTime] = useState(Date.now())
+  const [rate,setRate]= useState(0)    
   const kValues = Array.from({ length: 20 }, (_, i) => i); // x-axis, tickets (0 to 19)
+
+  useEffect(()=>{
+    if(numberOfTickets==0){
+
+      setStartTime(Date.now())
+    }
+    
+  },[])
+  useEffect(()=>{
+    const updateRate = () => {
+      console.log("regular update")
+      console.log(numberOfTickets)
+      
+      const currentTime = Date.now();
+      const timeElapsed = (currentTime - startTime) / 10000; // Convert milliseconds to seconds
+      console.log(timeElapsed)
+      if (timeElapsed > 1) {
+        setRate(Math.floor(numberOfTickets / timeElapsed)); // Calculate tickets per second
+      }
+    };
+
+    // Update rate every second
+    const intervalId = setInterval(updateRate, 10000);
+    return () => clearInterval(intervalId)
+  },[numberOfTickets, startTime])
+
+  const handleClick = () => {
+    let newNumber = numberOfTickets+1
+    setNumberOfTickets(newNumber)
+   
+  };
 
   useEffect(() => {
     const generateProbabilities = () => {
-      const probabilities = kValues.map(k => poissonProbability(lambda, k));
+      const probabilities = kValues.map(k => poissonProbability(rate, k));
       setProbabilityData(probabilities);
     };
 
     generateProbabilities(); // Generate data when lambda changes
-  }, [lambda]);
+  }, [rate]);
 
   const data = {
     labels: kValues,
     datasets: [
       {
-        label: `Poisson Probability (λ = ${lambda})`,
+        label: `Poisson Probability (λ = ${rate})`,
         data: probabilityData,
         borderColor: 'rgba(75, 192, 192, 1)',
         backgroundColor: 'rgba(75, 192, 192, 0.2)',
@@ -80,10 +116,21 @@ function PoissonGraph({ lambda }) {
   };
 
   return (
-    <div>
+    <>
+    
+     <div>
       <h2 className="card-title">Poisson Distribution of Tickets</h2>
       <Line data={data} options={options} />
     </div>
+    <button onClick={handleClick} style={{
+          position: 'absolute', 
+          color: 'black', 
+          padding: '100px 20px', 
+          border: 'none', 
+          borderRadius: '5px',
+          cursor: 'pointer'
+        }}/>
+    </>
   );
 }
 
